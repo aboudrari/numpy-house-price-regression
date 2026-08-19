49d16b937dabff9682e01b3a4921841f02689972
ABDALLAH ABOUDRARI <aboudrariabdallah1@gmail.com>
2026-08-19 15:40:03 +0100

    Step 6: one_hot_encode

--- model.py
+++ model.py
@@ -60,8 +60,14 @@ def append_column(X, col):
     col_2d = col.reshape(-1, 1)
     return np.concatenate([X, col_2d], axis=1)
 
-# Step 6 - one_hot_encode (not yet solved)
-# TODO: implement
+# Step 6 - one_hot_encode
+def one_hot_encode(labels):
+    # TODO: Convert a 1-D array of categorical labels into a dense binary one-hot matrix.
+    #  N is the number of samples and C is the number of unique categories
+    labels = np.array(labels)
+    unique_ = np.unique(labels)
+    output = (labels[:, None] == unique_[None, :]).astype(float)
+    return output
 
 # Step 7 - fit_standardizer (not yet solved)
 # TODO: implement
